fe83644baad5a00fcf001df1980a82e3a9615ce6
Leo Chang <sinss0000@gmail.com>
2015-09-30 22:30:35 +0800

    Initial Commit

---
+++ LCLocationService/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCLocationService
+//
+//  Created by Leo on 9/30/15.
+//  Copyright © 2015 Perfectidea Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCLocationService/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCLocationService
+//
+//  Created by Leo on 9/30/15.
+//  Copyright © 2015 Perfectidea Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
